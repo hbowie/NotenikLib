9879aa143ded9ba383fa69ba91cb798b1c7d3f37
Herb Bowie <hbowie@mac.com>
2023-01-16 11:42:16 -0800

    Added Public Access to Sort Descending flag

--- Sources/NotenikLib/model/NoteCollection.swift
+++ Sources/NotenikLib/model/NoteCollection.swift
@@ -25,7 +25,7 @@ public class NoteCollection {
     public  var dict        : FieldDictionary
     public  var hasLookupFields = false
     public  var sortParm    : NoteSortParm
-            var sortDescending: Bool
+    public  var sortDescending: Bool
     public  var typeCatalog  = AllTypes()
     public  var statusConfig: StatusValueConfig
     public  var rankConfig:   RankValueConfig
