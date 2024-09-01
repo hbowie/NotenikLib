bcab7bfa97707465f51cbf4e247977efddb884b9
Herb Bowie <hbowie@mac.com>
2024-09-01 09:32:07 -0700

    Group Untagged Notes

--- Sources/NotenikLib/tags/TagsTree.swift
+++ Sources/NotenikLib/tags/TagsTree.swift
@@ -16,37 +16,39 @@ class TagsTree {
     
     let root = TagsNode()
     
+    let untagged = TagsValue("- untagged")
+    
     /// Add a note to the Tags Tree, with one leaf for each Tag that the note possesses
     func add(note: Note) {
+        var tags = TagsValue()
         if note.hasTags() {
-            let tags = note.tags
-            var i = 0
-            
-            // Process each tag separately
-            while i < tags.tags.count {
-                let tag = tags.tags[i]
-                var node = root
-                var j = 0
-                while j < tag.levels.count {
-                    // Now let's work our way up through the tag's levels,
-                    // adding (or locating) one tag level at a time, working
-                    // our way deeper into the tree structure as we go.
-                    let level = tag.levels[j]
-                    let nextNode = node.addChild(tagLevel: level)
-                    node = nextNode
-                    j += 1
-                }
-                
-                // Now that we've worked our way through the tags,
-                // Add the note itself to the tree.
-                _ = node.addChild(note: note)
-                
-                // And on to the next tag for this note
-                i += 1
-            }
+            tags = note.tags
         } else {
-            // If no tags for this note, then just add the note to the root node. 
-            _ = root.addChild(note: note)
+            tags = untagged
+        }
+        var i = 0
+        
+        // Process each tag separately
+        while i < tags.tags.count {
+            let tag = tags.tags[i]
+            var node = root
+            var j = 0
+            while j < tag.levels.count {
+                // Now let's work our way up through the tag's levels,
+                // adding (or locating) one tag level at a time, working
+                // our way deeper into the tree structure as we go.
+                let level = tag.levels[j]
+                let nextNode = node.addChild(tagLevel: level)
+                node = nextNode
+                j += 1
+            }
+            
+            // Now that we've worked our way through the tags,
+            // Add the note itself to the tree.
+            _ = node.addChild(note: note)
+            
+            // And on to the next tag for this note
+            i += 1
         }
     }
     
